9e5d06b27e998d70d131d9cbaa1944592ac51680
percygr <percyg@gmail.com>
2023-05-25 08:36:55 +0100

    crappy frontend and send & delete, unless i pushed this earlier

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,6 +13,9 @@ const supabase = createClient(
 function App() {
   const [session, setSession] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("");
+  const [recipient, setRecipient] = useState("");
 
   // getMessages();
 
@@ -21,6 +24,7 @@ function App() {
       setSession(session);
       if (session) {
         getMessages(session);
+        getUsers();
       }
     });
 
@@ -30,6 +34,7 @@ function App() {
       setSession(session);
       if (session) {
         getMessages(session);
+        getUsers();
       }
     });
 
@@ -44,12 +49,51 @@ function App() {
     if (error) {
       console.log("error", error);
     }
-    console.log("messages", messages);
     setMessages(messages);
+  }
+
+  async function getUsers() {
+    let { data: users, error } = await supabase.from("users").select("*");
+    if (error) {
+      console.log("error", error);
+    }
+    setUsers(users);
+  }
+
+  async function deleteMessage(messageId) {
+    let { data: messages, error } = await supabase
+      .from("messages")
+      .delete()
+      .eq("id", messageId);
+    if (error) {
+      console.log("error", error);
+    }
+    getMessages(session);
+  }
+
+  async function sendMessage(messageText, destinationId) {
+    let { data: messages, error } = await supabase.from("messages").insert([
+      {
+        message: messageText,
+        destination_id: destinationId,
+        author_id: session.user.id,
+      },
+    ]);
+    if (error) {
+      console.log("error", error);
+    }
+    //refresh the page
+    // window.location.reload();
+    getMessages(session);
   }
 
   const handleLogout = async () => {
     await supabase.auth.signOut();
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    sendMessage(message, recipient);
   };
 
   if (!session) {
@@ -63,18 +107,55 @@ function App() {
       </div>
     );
   } else {
-    // getMessages();
     return (
-      <div>
-        <div>Logged in!</div>
-        <p>Welcome, {session.user.email}</p>
-        <button onClick={handleLogout}>Log Out</button>
-        {messages.map((message) => (
-          <div key={message.id}>
-            <h3>{message.created_at}</h3>
-            <p>{message.message}</p>
+      <div className="container">
+        <div className="login-info">
+          <div>Logged in as {session.user.email}</div>
+          <button onClick={handleLogout}>Log Out</button>
+        </div>
+        <div className="bottom-panel">
+          <div className="send-message">
+            <form onSubmit={handleSubmit} className="send-form">
+              <p>Send a message</p>
+              <textarea
+                placeholder="Type Message Here"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <select
+                value={recipient}
+                onChange={(e) => setRecipient(e.target.value)}
+              >
+                <option value="">Select a recipient</option>
+                {users.map((user) => {
+                  return (
+                    <option key={user.id} value={user.id}>
+                      {user.email}
+                    </option>
+                  );
+                })}
+              </select>
+              <button type="submit">Send</button>
+            </form>
           </div>
-        ))}
+          <div className="right-panel">
+            {messages.map((message) => {
+              const user = users.find((user) => user.id === message.author_id);
+              return (
+                <div key={message.id}>
+                  <h3>{message.message}</h3>
+                  <p>From: {user.email}</p>
+                  <p>
+                    Sent: {new Date(message.created_at).toLocaleString("en-GB")}
+                  </p>
+                  <button onClick={() => deleteMessage(message.id)}>
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   }
